f7d47ad59388094e1a6064a9aa8f11782b39be3a
Tiago Ferreira <tferreira@galois.com>
2023-07-19 11:23:59 -0700

    feat: cleanup and robustness

--- server.py
+++ server.py
@@ -1,27 +1,20 @@
+import socketserver
 import socket
 
 HOST = "0.0.0.0"
 PORT = 44344
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, 1)
-    s.bind((HOST, PORT))
-    s.listen()
-    while True:
-        conn, addr = s.accept()
-        print("Accepting connections...")
-        with conn:
-            print(f"Connected by {addr}.")
-            while True:
-                try:
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    data = data.decode("utf-8").rstrip()
-                    print(f"{addr} --> {data}")
-                    print(f"{(HOST, PORT)} --> {data[::-1]}")
-                    conn.sendall((data[::-1] + "\n").encode())
-                except:
-                    print("Client disconnect...")
-                    continue
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    def handle(self):
+        while True:
+            self.data = self.request.recv(1024).strip()
+            self.request.send(self.data[::-1])
+
+if __name__ == "__main__":
+    server = socketserver.ThreadingTCPServer((HOST, PORT), MyTCPHandler)
+    server.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    server.socket.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, 1)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
