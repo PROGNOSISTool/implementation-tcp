8529b68734894a2c0a4c718df5971c27f836daeb
Tiago Ferreira <tferreira@galois.com>
2023-07-18 17:09:27 -0700

    feat: cleanup and robustness

--- server.py
+++ server.py
@@ -3,14 +3,14 @@ import socket
 HOST = "0.0.0.0"
 PORT = 44344
 
-while True:
-    print("Accepting connections...")
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
-        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, 1)
-        s.bind((HOST, PORT))
-        s.listen()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_QUICKACK, 1)
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
         conn, addr = s.accept()
+        print("Accepting connections...")
         with conn:
             print(f"Connected by {addr}.")
             while True:
